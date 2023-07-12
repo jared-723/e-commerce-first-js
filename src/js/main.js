ad68a007311c84264098016939815b860a59a613
jared-723 <jarwend723@gmail.com>
2023-07-12 02:07:58 +0000

    almost finish logic

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,10 @@ const dataBase = 'https://ecommercebackend.fundamentos-29.repl.co/';
 async function getApi(){
     try {
         const db = await fetch(dataBase);
-        const res = db.json();
+        const res = await db.json();
+
+        window.localStorage.setItem("products", JSON.stringify(res))
+
         return res;
     } catch (error) {
         console.log(error)
@@ -12,89 +15,216 @@ async function getApi(){
 
 function addCart(store){
     const accessStore = store.cart
-    const cartContetHTML = document.querySelector('.productsInCart');
-    const productHTML = document.querySelector('.card__product');
+
+    const productHTML = document.querySelector('.products');
 
     productHTML.addEventListener('click', function(e){
         let html = '';
             if(e.target.classList.contains('bx-plus-circle')){
-                store.products.forEach(({name, quantity, price, id, image}) => {
-                    if(e.target.id == id){
-                        html += `
-                        <div class="productInCart"> 
-                            <div class="imgProductCart">
-                                <img src="${image}" class="imgPCart"></img>
-                            </div>
-                            <div class="infoProductCart">
-                                <h3>${name}</h3>
-                                <div class="stockPriceCart">
-                                <p>Stock: ${quantity}</p>
-                                <p>$${price}</p>
-                                </div>
-                                <div class="subTotalCart"></div>
-                                <div class="addReduceProductCart"></div>
-                            </div>
-                        </div>
-                    `
-                    return html;
-                    }
-                });
-            cartContetHTML.innerHTML = html;
+                const id = Number(e.target.id);
+                const findProduct = store.products.find((product) => product.id === id);
+
+                if(accessStore[findProduct.id]){
+
+                    if(findProduct.quantity === accessStore[findProduct.id].amount) 
+                        return alert("no hay mas en stock")
+                        
+                    accessStore[findProduct.id].amount++;
+                }else {
+                    accessStore[findProduct.id] ={...findProduct, amount: 1}
+                }
+
+                window.localStorage.setItem("cart", JSON.stringify(accessStore))
+
+                printProductsInCart(store);
+                totalCart(store);
             }
     })
 }
 
+function plusMinusDeleteCart(store){
+    const productInCartHTML = document.querySelector('.productsInCart');
+    productInCartHTML.addEventListener('click', function(e){
+        if(e.target.classList.contains('bx-minus')){
+            const id= Number(e.target.parentElement.id);
+
+            if(store.cart[id].amount === 1){
+                const response = confirm ('Seguro quieres eliminarlo del carrito?')
+                if(!response) return;
+                delete store.cart[id];
+            }else{
+                store.cart[id].amount--;
+            }
+
+        }
+        if(e.target.classList.contains('bx-plus')){
+
+            const id= Number(e.target.parentElement.id);
+            const findProduct = store.products.find((product) => product.id === id)
+
+            if(findProduct.quantity === store.cart[findProduct.id].amount) 
+            return alert("no hay mas en stock")
+
+            store.cart[id].amount++;
+
+        }
+
+        if(e.target.classList.contains('bx-trash-alt')){
+            const id= Number(e.target.parentElement.id);
+            const response = confirm ('Seguro quieres eliminarlo del carrito?')
+            if(!response) return;
+            delete store.cart[id];
+        }
+
+        window.localStorage.setItem('cart', JSON.stringify(store.cart));
+        printProductsInCart(store);
+        totalCart(store);
+    })
+}
+
+function printProductsInCart(store){
+    
+    let html='';
+    for(const product in store.cart){
+    const {quantity, name, price, image, id, amount} = store.cart[product]
+    html += `
+        <div class="productInCart">
+            <div class="img__cart">
+                <img src="${image}" alt="img"></img>
+            </div>
+            <div class="info__cart">
+                <h4>${name} | $${price}</h4>
+                <p>Stock: ${quantity}</p>
+                    <div class="options__unit" id="${id}"> 
+                        <i class='bx bx-minus'></i>
+                        <span>${amount} unit</span>
+                        <i class='bx bx-plus'></i>
+                        <i class='bx bx-trash-alt'></i>
+                    </div>
+            </div>
+                
+        </div>
+    `
+    }
+    document.querySelector('.productsInCart').innerHTML = html;
+}
+
+function totalCart(store){
+
+    const itemsCart = document.querySelector('.itemsCart');
+    const moneyTotal = document.querySelector('.moneyTotal');
+
+
+    let totalProduct = 0;
+    let amountProduct = 0;
+
+    for (const product in store.cart) {
+        const{amount,price}=store.cart[product];
+        amountProduct += amount;
+        totalProduct += amount * price;
+    }
+
+    itemsCart.textContent = amountProduct + ' units';
+    moneyTotal.textContent = '$' + totalProduct + '.00';
+
+}
+
+function buyCart(store){
+    const btn__buyHTML = document.querySelector('.btn__buy');
+
+    btn__buyHTML.addEventListener('click', function(){
+
+        if(!Object.values(store.cart).length) 
+            return alert("Pero tienes que tener algo en el carrito, no?");
+
+        const response = confirm("Seguro que quiere comprar?");
+        if(!response) return;
+
+        const currentProducts = [];
+
+        for (const product of store.products) {
+            const productCart = store.cart[product.id]
+            if(product.id === productCart?.id){
+                currentProducts.push({
+                    ...product,
+                    quantity: product.quantity - productCart.amount
+                });
+            }else {
+                currentProducts.push(product)
+            }
+        }
+
+        store.products = currentProducts;
+        store.cart = {};
+
+        window.localStorage.setItem("products", JSON.stringify(store.products));
+        window.localStorage.setItem("cart", JSON.stringify(store.cart));
+
+        totalCart(store);
+        printProductsInCart(store);
+        prinProducts(store);
+    })
+}
+
 function showCartAndDarkMode(){
-    const cartHTML=document.querySelector('.cartContent');
+    const cartHTML=document.querySelector('.productsCart');
     const darkModeCartHTMl = document.querySelector('.darkModeCart');
 
     darkModeCartHTMl.addEventListener('click', function(e){
         if(e.target.classList.contains('bxs-cart')){
-            cartHTML.classList.add('cartContentShow')
+            document.querySelector('.cartContent').classList.add('cartContentShow')
         }
     })
     cartHTML.addEventListener('click', function(e){
         if(e.target.classList.contains('quitCart')){
-            cartHTML.classList.remove('cartContentShow');
+            document.querySelector('.cartContent').classList.remove('cartContentShow');
         }
     })
 }
 
-function prinProducts(e){
+function prinProducts(store){
     let html = '';
-    
-    e.products.forEach(({id, name, price, image, category, quantity}) => {
+    for (const product of store.products) {
         html += `
-            <div class ="product ${category} card__product">
+            <div class ="product_${product.category} card__product">
                 <div class="product__image">
-                    <img src="${image}" class="img__product"></img>
+                    <img src="${product.image}" class="img__product"></img>
                 </div>
                 <div class="product__info">
                     <div class ="price__stock">
-                        <i class='bx bx-plus-circle' id="${id}"></i>
-                        <h3 class="priceProduct">$ ${price}.00 </h3> 
-                        <p class="quantityProduct">Stock: ${quantity}</p>
+                        <h3 class="priceProduct">
+                        $ ${product.price}.00
+                        ${
+                            product.quantity ? 
+                            `<i class='bx bx-plus-circle' id="${product.id}"></i>` 
+                            : "<span class='soldOut'>Sold Out</span>"
+                        } 
+                        </h3> 
+                        <p class="quantityProduct">Stock: ${product.quantity}</p>
                     </div>
-                        <p class="product__description" id="${id}"> 
-                        ${name}
+                        <p class="product__description" id="${product.id}"> 
+                        ${product.name}
                         </p>
                 </div>
             </div>
         `
-
-    });
+    };
     document.querySelector('#pr0ductsApi').innerHTML = html;
 }
 
 
 async function main(){
     const store = {
-        products: await getApi(),
-        cart: {}
+        products: JSON.parse(window.localStorage.getItem("products")) || await getApi(),
+        cart: JSON.parse(window.localStorage.getItem("cart")) || {}
     }
     prinProducts(store);
     showCartAndDarkMode();
     addCart(store);
+    printProductsInCart(store);
+    plusMinusDeleteCart(store);
+    totalCart(store);
+    buyCart(store);
 }
 
 
